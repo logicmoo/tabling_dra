c491188ac2d511ff91b621113ae87b03e55f768a
Feliks Kluzniak <feliks.kluzniak@utdallas.edu>
2009-01-29 18:54:30 -0600

    A general reworking of the LTL stuff: extended interpreter, using operators now, more examples

---
+++ coind/ltl_interpreter.clp
@@ -0,0 +1,121 @@
+%% Gopal Gupta's LTL interpreter (modified by F.K.).
+
+:- op( 10,  fy , ~   ).   % not
+:- op( 20, xfy , ^   ).   % and
+:- op( 30, xfy , v   ).   % or
+:- op( 10,  fy , x   ).   % LTL: "next"
+:- op( 10,  fy , f   ).   % LTL: "eventually"
+:- op( 10,  fy , g   ).   % LTL: "always"
+:- op( 20, xfx , u   ).   % LTL: "until"
+:- op( 20, xfx , r   ).   % LTL: "release"
+
+:- [ 'ltl_normalize.pl' ].
+
+
+:- top check/2.          % make check( S, F ) available in its original form
+
+% Don't transform these:
+:- bottom holds/2, normalize/2, proposition/1, state/1, trans/2, trans_all/2.
+
+
+% A single transition:
+
+trans( X, Y ) :-  trans_all( X, Set ),  member( Y, Set ).
+
+
+
+%% Check whether the state satisfies the formula.
+
+check( State, Formula ) :-
+        check_consistency,
+        normalize( Formula, NormalFormula ),
+        once( verify( State, Formula ) ).
+
+
+% Check the consistency of the automaton's description.
+
+check_consistency :-
+        check_propositions,
+        check_transitions.
+
+% Make sure propositions don't clash with operators.
+
+check_propositions :-
+        ( proposition( 'v' ) 
+        ; proposition( 'x' )
+        ; proposition( 'f' )
+        ; proposition( 'g' )
+        ; proposition( 'u' )
+        ; proposition( 'r' )
+        ),
+        !,
+        write( user_error,
+          '\"v\", \"x\", \"f\", \"g\", \"u\" and \"r\" cannot be propositions!'
+             ),
+        nl( user_error ),
+        fail.
+
+check_propositions.
+
+
+% Make sure that there is no state with no outgoing transitions, and that all 
+% transitions are between states.
+
+check_transitions :-
+        trans( S1, S2 ),
+        (var( S1 ) ;  var( S2 ) ; \+ state( S1 ) ; \+ state( S2 )),
+        !,
+        write( user_error, 'Transitions can only occur between states: ' ),
+        write( user_error, S1 ),
+        write( user_error, ' ---> ' ),
+        write( user_error, S2 ),
+        nl( user_error),
+        fail.
+
+check_transitions :-
+        state( S ),
+        (\+ trans( S, Set ) ; trans( S, [] )),
+        !,
+        write( user_error, 'No transition out of state ' ),
+        write( user_error, S ),
+        nl( user_error ),
+        fail.
+
+check_transitions.    
+
+
+
+%--- The formula is normalized: the nots have been pushed down to propositions.
+
+verify_all( []                , _ ).
+verify_all( [ State | States ], A ) :-  verify( State, A ),
+                                        verify_all( States, A ).
+
+:- tabled verify/2.
+
+verify( S, A     ) :-  proposition( A ), holds( S, A ).
+
+verify( S, ~ A   ) :-  proposition( A ), \+ holds( S, A ).
+
+verify( S, A ^ B ) :-  verify( S, A ) , verify( S, B ).
+
+verify( S, A v B ) :-  verify( S, A ) ; verify( S, B ).
+
+verify( S, x A   ) :-  trans_all( S, Set ), verify_all( Set, A ). 
+
+verify( S, f A   ) :-  verify( S, A ) ; verify( S, x f A ).
+
+verify( S, g A   ) :-  once( coverify( S, g A ) ).
+
+verify( S, A u B ) :-  verify( S, B ) ; verify( S, A ^ x( A u B) ).
+
+verify( S, A r B ) :-  once( coverify( S, A r B ) ).
+
+
+:- coinductive coverify/2.
+
+coverify( S, g A   ) :-  verify( S, A ^ x g A ).
+
+coverify( S, A r B ) :-  verify( S, A ^ B ).
+
+coverify( S, A r B ) :-  verify( S, B ^ x( A r B ) ).
--- coind/ltl_normalize.pl
+++ coind/ltl_normalize.pl
@@ -1,75 +1,82 @@
 %--- Normalize an LTL formula by pushing negations to the level of propositions
 %--- and applying some absorption and idempotency laws.
+%---
+%--- NOTE: see ltl_interpreter for the declarations of operators.
 
-normalize( not( or( A, B ) ), Normalized ) :-
-        normalize( and( not( A ), not( B ) ), Normalized ).
 
-normalize( not( and( A, B ) ), Normalized ) :-
-        normalize( or( not( A ), not( B ) ), Normalized ).
 
-normalize( not( not( A ) ), Normalized ) :-
-        normalize( A, Normalized ).
+normalize( ~( A v B ) , Normalized ) :-
+        normalize( ~ A ^ ~ B , Normalized ).
 
-normalize( not( x( A ) ), Normalized ) :-
-        normalize( x( not( A ) ), Normalized ).
+normalize( ~( A ^ B ) , Normalized ) :-
+        normalize( ~ A v ~ B , Normalized ).
 
-normalize( not( f( A ) ), Normalized ) :-
-        normalize( g( not( A ) ), Normalized ).
+normalize( ~ ~ A , Normalized ) :-
+        normalize( A , Normalized ).
 
-normalize( not( g( A ) ), Normalized ) :-
-        normalize( f( not( A ) ), Normalized ).
+normalize( ~ x A , Normalized ) :-
+        normalize( x ~ A , Normalized ).
 
-normalize( not( u( A, B ) ), Normalized ) :-
-        normalize( r( not( A ), not( B ) ), Normalized ).
+normalize( ~ f A , Normalized ) :-
+        normalize( g ~ A , Normalized ).
 
-normalize( not( r( A, B ) ), Normalized ) :-
-        normalize( u( not( A ), not( B ) ), Normalized ).
+normalize( ~ g A , Normalized ) :-
+        normalize( f ~ A , Normalized ).
 
-normalize( or( A, B ), or( NA, NB ) ) :-
+normalize( ~( A u B ) , Normalized ) :-
+        normalize( ~ A r ~ B , Normalized ).
+
+normalize( ~( A r B ) , Normalized ) :-
+        normalize( ~ A u ~ B , Normalized ).
+
+normalize( A v B , NA v NB ) :-
         normalize( A, NA ),
         normalize( B, NB ).
 
-normalize( and( A, B ), and( NA, NB ) ) :-
+normalize( A ^ B , NA ^ NB ) :-
         normalize( A, NA ),
         normalize( B, NB ).
 
-normalize( not( A ), not( NA ) ) :-
+normalize( ~ A , ~ NA ) :-
         normalize( A, NA ).
 
-normalize( x( A ), x( NA ) ) :-
+normalize( x A , x NA ) :-
         normalize( A, NA ).
 
-normalize( f( A ), f( NA ) ) :-
+normalize( f A , f NA ) :-
         normalize( A, NA ).
 
-normalize( g( A ), g( NA ) ) :-
+normalize( g A , g NA ) :-
         normalize( A, NA ).
 
-normalize( u( A, B ), u( NA, NB ) ) :-
+normalize( A u B , NA u NB ) :-
         normalize( A, NA ),
         normalize( B, NB ).
 
-normalize( r( A, B ), r( NA, NB ) ) :-
+normalize( A r B , NA r NB ) :-
         normalize( A, NA ),
         normalize( B, NB ).
 
-normalize( f( f( A ) ), Normalized ) :-
-        normalize( f( A ), Normalized ).
+normalize( f f A , Normalized ) :-
+        normalize( f A , Normalized ).
 
-normalize( g( g( A ) ), Normalized ) :-
-        normalize( g( A ), Normalized ).
+normalize( g g A , Normalized ) :-
+        normalize( g A , Normalized ).
 
-normalize( u( A, u( A, B ) ), Normalized ) :-
-        normalize( u( A, B ), Normalized  ).
+normalize( A u (A u B) , Normalized ) :-
+        normalize( A u B , Normalized  ).
 
-normalize( u( u( A, B ), B ), Normalized ) :-
-        normalize( u( A, B ), Normalized  ).
+normalize( (A u B) u B , Normalized ) :-
+        normalize( A u B , Normalized  ).
 
-normalize( f( g( f( A ) ) ), Normalized ) :-
-        normalize( g( f( A ) ), Normalized ).
+normalize( f g f A , Normalized ) :-
+        normalize( g f A , Normalized ).
 
-normalize( g( f( g( A ) ) ), Normalized ) :-
-        normalize( f( g( A ) ), Normalized ).
+normalize( g f g A , Normalized ) :-
+        normalize( f g A , Normalized ).
+
+normalize( ~ P, ~ P ) :-
+        proposition( P ).
 
 normalize( P, P ) :-
         proposition( P ).
