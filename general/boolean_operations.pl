7de6904860bcc4812cf144632ed0f86bb9345d78
Feliks Kluzniak <feliks.kluzniak@utdallas.edu>
2009-03-27 19:05:01 -0500

    Added the copyright notice

--- general/boolean_operations.pl
+++ general/boolean_operations.pl
@@ -1,3 +1,26 @@
+   % NOTICE: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   %                                                                      %
+   %  COPYRIGHT (2009) University of Dallas at Texas.                     %
+   %                                                                      %
+   %  Developed at the Applied Logic, Programming Languages and Systems   %
+   %  (ALPS) Laboratory at UTD by Feliks Kluzniak.                        %
+   %                                                                      %
+   %  Permission is granted to modify this file, and to distribute its    %
+   %  original or modified contents for non-commercial purposes, on the   %
+   %  condition that this notice is included in all copies in its         %
+   %  original form.                                                      %
+   %                                                                      %
+   %  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,     %
+   %  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES     %
+   %  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND     %
+   %  NON-INFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR        %
+   %  ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE FOR ANY DAMAGES OR       %
+   %  OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, ARISING    %
+   %  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR       %
+   %  OTHER DEALINGS IN THE SOFTWARE.                                     %
+   %                                                                      %
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%%  Utilities for performing operations on boolean expressions.             %%%
 %%%                                                                          %%%
 %%%  Written by Feliks Kluzniak at UTD (March 2009).                         %%%
