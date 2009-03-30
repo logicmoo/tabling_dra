1f66aef6f157053a0c29f5818cfcf9f6c0a98fc5
Feliks Kluzniak <feliks.kluzniak@utdallas.edu>
2009-03-30 16:10:22 -0500

    A very small example for coinduction

---
+++ tabling/examples/LTL/coind_example.pl
@@ -0,0 +1,17 @@
+:- [ 'verifier.pl' ].
+
+proposition( p ).
+proposition( q ).
+
+state( s0 ).
+
+trans( s0, s0 ).
+
+holds( s0, q ).
+
+
+%                                         Expected   Prolog    Tabling
+
+q :- check( s0, f p ).                 %  no         *         no
+
+                                       % (*) loops when writing counterexample
