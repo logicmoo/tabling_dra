b2a27350919bb954754b35a1c4ccde1499dd2eca
DouglasRMiles <logicmoo@gmail.com>
2016-02-13 00:39:39 -0800

    adding in pack.pl

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(dra_tabling).
+version('1.0.1').
+title('SWI-Prolog interface to Table-handling procedures for the "dra" interpreter. Written by Feliks Kluzniak at UTD (March 2009)').
+keywords([tabling,memoization]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/logicmoo/swicli' ).
+download( 'https://github.com/TeamSPoon/dra_tabling/release/*.zip' ).
+requires(logicmoo_base).
